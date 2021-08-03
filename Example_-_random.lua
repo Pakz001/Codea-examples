3cc9b3ef7881a86afd68cd96248ac8aed978ae62
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-08-03 10:34:17 +0200

    Create Example_-_random.lua

---
+++ Example_-_random.lua
@@ -0,0 +1,25 @@
+-- Test
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+    -- print random values
+    print(math.random()) -- float
+    print(math.random(0,2)) -- integers, inclusive
+end
+
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    
+    
+    -- Do your drawing here
+    
+end
+
