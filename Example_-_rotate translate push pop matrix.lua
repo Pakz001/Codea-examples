dc7bd92129d0fe436141f442bec79bf3d0ec5eee
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-08-03 11:55:35 +0200

    Update Example_-_rotate translate push pop matrix.lua

--- Example_-_rotate translate push pop matrix.lua
+++ Example_-_rotate translate push pop matrix.lua
@@ -1,1 +1,25 @@
 
+-- Test
+angle=0
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    angle=angle+1
+    if angle>359 then
+    angle=0
+    end
+    
+    pushMatrix()
+    translate(200,HEIGHT-100)
+    rotate(angle)
+    rectMode(CENTER)
+    rect(0,0,100,100)
+    popMatrix()
+end
