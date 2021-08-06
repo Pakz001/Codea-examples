4c8876f3f8413f496c8733b2b036fb7dfd02802e
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-08-06 21:31:48 +0200

    Create Beginners_-_vector negative.lua

---
+++ Beginners_-_vector negative.lua
@@ -0,0 +1,23 @@
+-- negative vector
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+    -- create a new vector in a with the negative values from the set vector
+    a = -vec2(10,10)
+    -- print out the vector values inside the console window.
+    print(a)
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing here
+    
+end
+
