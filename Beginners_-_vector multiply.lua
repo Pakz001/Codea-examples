69862b674252c6ee7004ae10f2ecb242e9b74219
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-08-06 21:24:00 +0200

    Create Beginners_-_vector multiply.lua

---
+++ Beginners_-_vector multiply.lua
@@ -0,0 +1,24 @@
+-- vector multiply
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+    a = vec2(10,10)
+    -- you can multiply the vectors x,y by a value
+    a = a * 10.5
+    -- print the new vector values
+    print(a)
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing here
+    
+end
+
