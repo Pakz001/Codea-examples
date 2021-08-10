5814a2ecdedf40b2fa462696e5fbb4d9df1dc360
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2021-08-10 14:06:49 +0200

    Update Beginners_-_vector angle.lua

--- Beginners_-_vector angle.lua
+++ Beginners_-_vector angle.lua
@@ -10,6 +10,13 @@ function setup()
     
     -- get the angle between two vectors.
     -- note that the upper case B is required!
+    
+    -- another inportant note is
+    -- that it takes the first angle
+    -- from position 0,0
+    -- you would need to set up the
+    -- vectors like you would
+    -- set up atan2
     a = vec2(100,100):angleBetween(vec2(200,0))
     -- print out the angle in radians and in degrees
     print(a)
@@ -20,11 +27,10 @@ end
 function draw()
     -- This sets a dark background color 
     background(40, 40, 50)
-
+    
     -- This sets the line thickness
     strokeWidth(5)
-
+    
     -- Do your drawing here
     
 end
-
