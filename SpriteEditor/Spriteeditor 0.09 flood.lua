7ff64fd162675cee936e3fbe464a74d3021e9c69
Rudy Boudewijn van Etten <cromdesign@gmail.com>
2022-01-17 20:42:24 +0100

    Update Spriteeditor 0.09 flood.lua

--- SpriteEditor/Spriteeditor 0.09 flood.lua
+++ SpriteEditor/Spriteeditor 0.09 flood.lua
@@ -60,7 +60,7 @@ end
 -- This function gets called once every frame
 function draw()
     -- This sets a dark background color 
-    background(40, 40, 50)
+    background(0)
     
     -- This sets the line thickness
     strokeWidth(5)
